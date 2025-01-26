927e352b276b57989ca75a05803f035787b56f7b
teresacabeza <teresa.cabeza@alumnos.upm.es>
2025-01-26 20:32:06 +0100

    indexado

--- manageLib.py
+++ manageLib.py
@@ -34,22 +34,22 @@ def docker_destroy():
 # 3. DESPLIEGUE DE LA APLICACIÓN USANDO DOCKER-COMPOSE
  def mv_docker_compose ():
 
-call(['sudo', 'apt-get', 'install', '-y', 'docker.io'])
-call(['sudo', 'apt-get', 'install', '-y', 'docker-compose'])
+  call(['sudo', 'apt-get', 'install', '-y', 'docker.io'])
+  call(['sudo', 'apt-get', 'install', '-y', 'docker-compose'])
 
 #Instalaciones en la máquina virtual
-call(['sudo', 'apt-get', 'upgrate'])
-call(['sudo', 'apt-get', 'install', '-y', 'python3-pip']) 
-call(['sudo', 'apt-get', 'install', '-y','git'])
-call(['git', 'clone', 'https://github.com/CDPS-ETSIT/practica_creativa2.git'])
-call(['sudo', 'apt-get', 'update'])
+  call(['sudo', 'apt-get', 'upgrate'])
+  call(['sudo', 'apt-get', 'install', '-y', 'python3-pip']) 
+  call(['sudo', 'apt-get', 'install', '-y','git'])
+  call(['git', 'clone', 'https://github.com/CDPS-ETSIT/practica_creativa2.git'])
+  call(['sudo', 'apt-get', 'update'])
 
 
 #Ejecutar el comando que dice el enunciado en la ruta src/reviews:				
-os.chdir('practica_creativa2/bookinfo/src/reviews')
-os.system('sudo docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build')
-os.chdir(os.path.expanduser("~"))
-os.chdir('practicaCreativa2/apartado3')
+  os.chdir('practica_creativa2/bookinfo/src/reviews')
+  os.system('sudo docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build')
+  os.chdir(os.path.expanduser("~"))
+  os.chdir('practicaCreativa2/apartado3')
 #print("Directorio actual")
 #subprocess.run(['pwd'])
 #print("Archivos en el directorio actual")
@@ -58,8 +58,8 @@ os.chdir('practicaCreativa2/apartado3')
 
 #Situarnos en la carpeta donde se encuentra docker-compose.yaml e iniciar los servicios definidos en ese archivo
 #os.chdir('practica_creativa2/bookinfo/src')
-call(['sudo', 'docker-compose', 'build'])
-call(['sudo', 'docker-compose', 'up', '-d'])
+  call(['sudo', 'docker-compose', 'build'])
+  call(['sudo', 'docker-compose', 'up', '-d'])
   
 #   # Clonar repositorio de la app
 #   # subprocess.call(['git', 'clone', 'https://github.com/CDPS-ETSIT/practica_creativa2.git', './bloque3'])
