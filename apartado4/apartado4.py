bc256f33cdcf5389b6e412e8b65e408ea732dfe8
teresacabeza <teresa.cabeza@alumnos.upm.es>
2025-01-26 18:23:43 +0100

    apartado4.py

--- apartado4/apartado4.py
+++ apartado4/apartado4.py
@@ -1,39 +1,32 @@
-#!usr/bin/python3
 import os
+import sys
 
-#Descarga las imagenes del repositorio Docker Hub
-os.system('sudo docker pull jorgerguezz/details:16')
-os.system('sudo docker pull jorgerguezz/productpage:16')
-os.system('sudo docker pull jorgerguezz/reviews:16')
-os.system('sudo docker pull jorgerguezz/ratings:16')
+def build(version):
+    os.system('gcloud container clusters get-credentials clusterk --region europe-southwest1 --project norse-block-448119-j1')
+    os.system('kubectl apply -f productpage.yaml')
+    os.system('kubectl apply -f ratings.yaml')
+    os.system('kubectl apply -f details.yaml')
+    os.system('kubectl apply -f reviews-svc.yaml')
+    os.system(f'kubectl apply -f reviews-{version}-deployment.yaml')
 
-#Configuracion para usar Docker con Kubernetes
-os.system('sudo apt-get remove docker docker-engine docker.io containerd runc')
-os.system('sudo apt-get update')
-os.system('sudo apt-get install -y ca-certificates curl gnupg lsb-release')
-os.system('curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg')
-os.system('echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null')
-os.system('sudo apt-get update')
-os.system('sudo apt-get install -y docker-ce docker-ce-cli containerd.io')
+def delete():
+    os.system('kubectl delete --all deployments && kubectl delete --all pods && kubectl delete --all services')
 
-#Establece el proyecto de Google Cloud Platform y habilita el servicio de Kubernetes Engine en Google Cloud Platform
-os.system('sudo gcloud config set project norse-block-448119-j1')     
-os.system('sudo gcloud services enable container.googleapis.com')
+param = sys.argv
 
+if len(param) < 2:
+    print("Usage: python3 bloque4.py [build|delete] [version]")
+    sys.exit(1)
 
-# Crea los pods a partir de los archivos de configuracion .yaml (sino funciona añadir --disk-size=20)
-os.system('sudo gcloud container clusters create clusterkubernetes --num-nodes=3 --zone=europe-southwest1-c --no-enable-autoscaling') 
+command = param[1]
 
-
-os.system('sudo kubectl apply -f productpage.yaml')
-os.system('sudo kubectl apply -f details.yaml')
-os.system('sudo kubectl apply -f ratings.yaml')
-os.system('sudo kubectl apply -f reviews-service.yaml')
-
-# Se puede elegir la version que queramos 
-#os.system('sudo kubectl apply -f reviews-v1-deployment.yaml')
-#os.system('sudo kubectl apply -f reviews-v2-deployment.yaml')
-os.system('sudo kubectl apply -f reviews-v3-deployment.yaml')
-
-# Lanzamos el servicio
-os.system('sudo kubectl expose deployment productpage --type=LoadBalancer --port=9080')
+if command == "build":
+    if len(param) < 3:
+        print("Please specify a version (e.g., v1, v2, v3)")
+        sys.exit(1)
+    version = param[2]
+    build(version)
+elif command == "delete":
+    delete()
+else:
+    print("Unknown command")
